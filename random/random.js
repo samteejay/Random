afcb9016235550020194bc7309df8d0b35054e28
Samuel Tijani <omalitijani@gmail.com>
2020-02-10 14:57:11 +0100

    Init commit

---
+++ random/random.js
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>DICE</title>
+</head>
+
+<body>
+<script>
+
+var bigDecimal = Math.random();
+var ImprovedNumber = (bigDecimal * 6) + 1;
+var Dice = Math.floor(ImprovedNumber);
+
+alert(Dice);
+	
+</script>
+
+</body>
+</html>
